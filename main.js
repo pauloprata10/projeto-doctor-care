89a33c3d39a3f5d1054f7aee25ea7bb31478d2cf
Paulo Prata <pauloprata10@gmail.com>
2022-05-05 21:43:50 -0300

    subindo projeto para o github

---
+++ main.js
@@ -0,0 +1,19 @@
+// String (textos)
+// Number (número)
+// Boolean (true or false)
+
+function onScroll () {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
