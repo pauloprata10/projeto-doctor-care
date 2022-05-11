0d78e129163fa487ba2fe7829ed6894fdfc3128b
Paulo Prata <pauloprata10@gmail.com>
2022-05-11 20:22:56 -0300

    finalizando javascript do projeto

--- main.js
+++ main.js
@@ -8,6 +8,56 @@ onScroll()
 function onScroll () {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  //a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // informações dos dados e da lógica
+  console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetline)
+
+  // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+  
+  
+  // a seção termina onde ?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  console.log('O topo da seção passou da linha?', sectionEndPassedTargetline)
+
+  // limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline 
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector (`.menu a [href*=${sectionId}]`)
+
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
 }
 
 function showNavOnScroll () {
@@ -47,3 +97,9 @@ ScrollReveal({
 #about,
 #about header,
 #about .content`)
+
+function sayMyName(name) {
+  console.log(nome)
+}
+
+sayMyName("Mayk")
